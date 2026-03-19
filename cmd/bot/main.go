33f4c4e29756ac61161afb978956b106969b457d
asta <hadesandgo@gmail.com>
2026-03-19 09:58:29 +0100

    set webhook

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/KernelH132/pingme/internal/handler"
 	"github.com/KernelH132/pingme/internal/repository"
@@ -17,10 +18,48 @@ func init() {
 	if err != nil {
 		log.Println("No .env file found, relying on system environment variables")
 	}
+
+}
+
+func setTelegramWebhook() {
+	token := os.Getenv("BOT_TOKEN")
+	if token == "" {
+		log.Fatal("telegram bot token not set in environment")
+	}
+	publicURL := os.Getenv("RAILWAY_PUBLIC_DOMAIN")
+	if publicURL == "" {
+		log.Fatal("RAILWAY_PUBLIC_DOMAIN environment variable is not set")
+	}
+	webhookURL := fmt.Sprintf("https://%s/", publicURL)
+	apiURL := fmt.Sprintf("https://api.telegram.org/bot%s/setWebhook?url=%s", token, webhookURL)
+
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		log.Fatalf("Failed to set Telegram webhook: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Failed to set Telegram webhook, status code: %d", resp.StatusCode)
+	}
+
+	log.Println("Telegram webhook set successfully")
 }
 
 func main() {
 	repository.Connect()
-	fmt.Println("Server running on :8080")
-	http.ListenAndServe(":8080", http.HandlerFunc(handler.Handler))
+
+	go setTelegramWebhook()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("Server running on :" + port)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler.Handler)
+
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
